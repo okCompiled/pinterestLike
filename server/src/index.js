91999a8e1bb4874ccea76f1f49918619852f5a26
karishma <pkarishma.khan@essentia.dev>
2023-09-03 23:34:08 +0530

    allow cors

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,9 @@ const PORT = process.env.PORT || 8080;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morganMiddleware);
